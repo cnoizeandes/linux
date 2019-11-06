79bf2295ac70651b4efe0b93309c0cd0e76cadf9
Nick Hu <nickhu@andestech.com>
2019-11-06 11:27:04 +0800

    riscv: light/deep sleep for SMU.
    Let SMU to support kernel Suspend feature.
    
    Suspend to standby == light sleep in SMU.
    Suspend to ram == Deep sleep in SMU.
    
    Signed-off-by: Nick Hu <nickhu@andestech.com>

---
+++ arch/riscv/platforms/ae350/atcsmu.c
@@ -0,0 +1,229 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/of.h>
+
+#include <asm/tlbflush.h>
+#include <asm/sbi.h>
+#include <asm/andesv5/smu.h>
+
+struct atc_smu {
+        void __iomem *base;
+        struct resource *res;
+        spinlock_t lock;
+};
+struct atc_smu atcsmu;
+
+int get_pd_type(unsigned int cpu)
+{
+	struct atc_smu *smu = &atcsmu;
+	unsigned int val = readl((void *)(smu->base +
+				CN_PCS_STATUS_OFF(cpu)));
+
+	return GET_PD_TYPE(val);
+}
+
+int get_pd_status(unsigned int cpu)
+{
+	struct atc_smu *smu = &atcsmu;
+	unsigned int val = readl((void *)(smu->base +
+				CN_PCS_STATUS_OFF(cpu)));
+
+	return GET_PD_STATUS(val);
+}
+
+void set_wakeup_enable(int cpu, unsigned int events)
+{
+	struct atc_smu *smu = &atcsmu;
+
+	if (cpu == 0)
+		events |= (1 << PCS_WAKE_DBG_OFF);
+	writel(events, (void *)(smu->base + CN_PCS_WE_OFF(cpu)));
+}
+
+void set_sleep(int cpu, unsigned char sleep)
+{
+	struct atc_smu *smu = &atcsmu;
+	unsigned int val = readl((void *)(smu->base + CN_PCS_CTL_OFF(cpu)));
+	unsigned char *ctl = (unsigned char *)&val;
+
+	// set sleep cmd
+	*ctl = 0;
+	*ctl = *ctl | SLEEP_CMD;
+	// set param
+	*ctl = *ctl | (sleep << PCS_CTL_PARAM_OFF);
+	writel(val, (void *)(smu->base + CN_PCS_CTL_OFF(cpu)));
+
+	pr_debug("PCS%d after setting up PCS_CTL register:\n"
+	       "PCS_WE: 0x%x\n"
+	       "The value wants to write into PCS_CTL:%x\n"
+	       "PCS_CTL: 0x%x\n"
+	       "PCS_STATUS: 0x%x\n",
+		cpu + 3,
+		readl((void *)(smu->base + CN_PCS_WE_OFF(cpu))),
+		val,
+		readl((void *)(smu->base + CN_PCS_CTL_OFF(cpu))),
+		readl((void *)(smu->base + CN_PCS_STATUS_OFF(cpu))));
+
+}
+void sbi_disable_dcache(void *info)
+{
+	SBI_CALL_1(SBI_DCACHE_OP, 0);
+}
+
+extern int num_cpus;
+void andes_suspend2ram(void)
+{
+	unsigned int cpu, status, type;
+	int ready_cnt = num_cpus - 1;
+	int ready_cpu[NR_CPUS] = {0};
+#ifdef CONFIG_SMP
+	int id = smp_processor_id();
+#else
+	int id = 0;
+#endif
+	// Disable higher privilege's non-wakeup event
+	SBI_CALL_2(SBI_SUSPEND_PREPARE, 1, 0);
+
+	// polling SMU other CPU's PD_status
+	while (ready_cnt) {
+		for (cpu = 0; cpu < num_cpus; cpu++) {
+			if (cpu == id || ready_cpu[cpu] == 1)
+				continue;
+
+			type = get_pd_type(cpu);
+			status = get_pd_status(cpu);
+
+			if(type == SLEEP && status == DeepSleep_STATUS) {
+				ready_cnt--;
+				ready_cpu[cpu] = 1;
+			}
+		}
+	}
+
+	// set SMU wakeup enable & MISC control
+	set_wakeup_enable(id, *wake_mask);
+	// set SMU light sleep command
+	set_sleep(id, DeepSleep_CTL);
+	// backup, suspend and resume
+	SBI_CALL_0(SBI_SUSPEND_MEM);
+	// enable privilege
+	SBI_CALL_2(SBI_SUSPEND_PREPARE, 1, 1);
+}
+
+void andes_suspend2standby(void)
+{
+	unsigned int cpu, status, type;
+	int ready_cnt = num_cpus - 1;
+	int ready_cpu[NR_CPUS] = {0};
+#ifdef CONFIG_SMP
+	int id = smp_processor_id();
+#else
+	int id = 0;
+#endif
+	// Disable higher privilege's non-wakeup event
+	SBI_CALL_2(SBI_SUSPEND_PREPARE, 1, 0);
+
+	// polling SMU other CPU's PD_status
+	while (ready_cnt) {
+		for (cpu = 0; cpu < num_cpus; cpu++) {
+			if (cpu == id || ready_cpu[cpu] == 1)
+				continue;
+
+			type = get_pd_type(cpu);
+			status = get_pd_status(cpu);
+
+			if(type == SLEEP && status == LightSleep_STATUS) {
+				ready_cnt--;
+				ready_cpu[cpu] = 1;
+			}
+		}
+	}
+	// set SMU wakeup enable & MISC control
+	set_wakeup_enable(id, *wake_mask);
+
+	// set SMU light sleep command
+	set_sleep(id, LightSleep_CTL);
+
+	// flush dcache & dcache off
+	SBI_CALL_1(SBI_DCACHE_OP, 0);
+
+	// wfi
+	__asm__ volatile ("wfi\n\t");
+
+	// enable D-cache
+	SBI_CALL_1(SBI_DCACHE_OP, 1);
+
+	// enable privilege
+	SBI_CALL_2(SBI_SUSPEND_PREPARE, 1, 1);
+}
+
+static int atcsmu_probe(struct platform_device *pdev)
+{
+	struct atc_smu *smu = &atcsmu;
+	int ret = -ENOENT;
+	int pcs = 0;
+
+	spin_lock_init(&smu->lock);
+
+	smu->res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!smu->res)
+		goto err_exit;
+
+	ret = -EBUSY;
+
+	smu->res = request_mem_region(smu->res->start,
+					smu->res->end - smu->res->start+  1,
+					pdev->name);
+	if (!smu->res)
+		goto err_exit;
+
+	ret = -EINVAL;
+
+	smu->base = ioremap_nocache(smu->res->start,
+					smu->res->end - smu->res->start + 1);
+	if (!smu->base)
+		goto err_ioremap;
+
+	for(pcs = 0; pcs < MAX_PCS_SLOT; pcs++)
+		writel(0xffdfffff, (void *)(smu->base + PCSN_WE_OFF(pcs)));
+
+	return 0;
+err_ioremap:
+	release_resource(smu->res);
+err_exit:
+	return ret;
+}
+
+static int __exit atcsmu_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+#ifdef CONFIG_OF
+static const struct of_device_id atcsmu_of_id_table[] = {
+	{ .compatible = "andestech,atcsmu" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, atcsmu_of_id_table);
+#endif
+
+static struct platform_driver atcsmu_driver = {
+	.probe = atcsmu_probe,
+	.remove = __exit_p(atcsmu_remove),
+	.driver = {
+		.name = "atcsmu",
+		.of_match_table = of_match_ptr(atcsmu_of_id_table),
+	},
+};
+
+static int __init atcsmu_init(void)
+{
+	int ret = platform_driver_register(&atcsmu_driver);
+
+	return ret;
+}
+subsys_initcall(atcsmu_init);
