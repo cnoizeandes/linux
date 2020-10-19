5b65b55bdcc0a447ebda05e6b98b31ca95f2ac7b
Nylon Chen <nylon7@andestech.com>
2020-10-19 17:13:48 +0800

    Bug 18635 - Limitation about trigger module for icount type
    Don't set icount before first receive ptrace event about target
    debugging from gdbserver.
    
    We can't know the gdbserver exit debugging, so we don't recover the
    bypass jump.
    
    ref:22df6a7856418c4987e53ea845788f526c99395f
    
    Signed-off-by: Zong Li <zong@andestech.com>

--- arch/riscv/kernel/ptrace.c
+++ arch/riscv/kernel/ptrace.c
@@ -15,6 +15,7 @@
 #include <linux/elf.h>
 #include <linux/regset.h>
 #include <asm/sbi.h>
+#include <asm/cacheflush.h>
 #include <linux/sched.h>
 #include <linux/sched/task_stack.h>
 #include <linux/tracehook.h>
@@ -146,9 +147,24 @@ long arch_ptrace(struct task_struct *child, long request,
 	return ret;
 }
 
+#define CNOP           0x0001
+#define CNOP_SIZE      0x2
+extern void bypass_singlestep(void);
+static void modify_bypass_to_nop(void)
+{
+	unsigned int nop = CNOP;
+	probe_kernel_write(&bypass_singlestep, &nop, CNOP_SIZE);
+	smp_mb();
+	flush_icache_range(&bypass_singlestep, &bypass_singlestep + CNOP_SIZE);
+}
+static bool is_bypass_singlestep = true;
 void user_enable_single_step(struct task_struct *child)
 {
 	set_tsk_thread_flag(child, TIF_SINGLESTEP);
+	if (is_bypass_singlestep) {
+		modify_bypass_to_nop();
+		is_bypass_singlestep = false;
+	}
 }
 
 void user_disable_single_step(struct task_struct *child)
