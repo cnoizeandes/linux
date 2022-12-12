9aec865db2dc4df58c84d25ce7c2ba8cea40b831
Charles Ci-Jyun Wu <dminus@andestech.com>
2022-12-12 11:04:31 +0800

    riscv: amp: remove tree command in the build_all.sh
    The tree command, which is not installed by default on
    different OS distributions.
    
    Signed-off-by: Charles Ci-Jyun Wu <dminus@andestech.com>

--- samples/andes_remoteproc/App/build_all.sh
+++ samples/andes_remoteproc/App/build_all.sh
@@ -11,8 +11,4 @@ echo $LDFLAGS
 make clean
 make
 
-tree ../Script
-rm ../Script/echo_test ../Script/mat_mul_demo ../Script/proxy_app
-tree ../Script
 cp echo_test mat_mul_demo proxy_app ../Script
-tree ../Script
