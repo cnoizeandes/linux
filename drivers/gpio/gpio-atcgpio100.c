3305590b3b563a904a71782f6665e2c75e66cc86
cnoizeandes <38971102+cnoizeandes@users.noreply.github.com>
2023-10-03 12:03:56 +0800

    Update gpio-atcgpio100.c for bug test.

--- drivers/gpio/gpio-atcgpio100.c
+++ drivers/gpio/gpio-atcgpio100.c
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021 Andes Technology Corporation.
+ */
+
 #include <linux/module.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
@@ -15,7 +20,7 @@
 #define PIN_PULL_TYPE			0x44
 #define INT_ENABLE			0x50
 #define INT_STATE			0x64
-#define INT_MODE(ch)			(0x54+((ch/8)<<2))
+#define INT_MODE(ch)			(0x54 + ((ch / 8) << 2))
 #define HIGH_LEVEL			2
 #define LOW_LEVEL			3
 #define NEGATIVE_EDGE			5
@@ -23,14 +28,16 @@
 #define DUAL_EDGE			7
 #define DEBOUNCE_ENABLE			0x70
 
-#define ATCGPIO100_VIRTUAL_IRQ_BASE	32
+#define ATCGPIO100_VIRTUAL_IRQ_BASE	0
+#define DEFAULT_PIN_NUMBER		16
 
 struct atcgpio_priv {
 	struct gpio_chip gc;
-	struct irq_chip	chip;
+	struct irq_domain *domain;
 	void __iomem *base;
-	int virq_base;
+	unsigned long virq_base;
 	spinlock_t lock;
+	unsigned int irq_parent;
 };
 
 #define GPIO_READL(offset, base)	\
@@ -46,42 +53,39 @@ static inline struct atcgpio_priv *atcgpio_irq_data_get_data(struct irq_data *d)
 	return gpiochip_get_data(chip);
 }
 
-static int atcirq_to_gpio(unsigned int irq)
+static int atcirq_to_gpio(unsigned long irq)
 {
-	return irq - gpio_to_irq(0);
-}
-
-static int atcgpio_to_irq(struct gpio_chip *gc, unsigned int offset)
-{
-	struct atcgpio_priv *priv;
-
-	priv = gpiochip_get_data(gc);
-
-	return priv->virq_base+ offset;
+	return irq - (ATCGPIO100_VIRTUAL_IRQ_BASE ? gpio_to_irq(0) : 0);
 }
 
 static int atcgpio_get(struct gpio_chip *gc, unsigned int gpio)
 {
 	struct atcgpio_priv *priv;
+	unsigned long flags;
+	u32 val;
 
 	priv = gpiochip_get_data(gc);
+	spin_lock_irqsave(&priv->lock, flags);
+	val = GPIO_READL(GPIO_DATA_IN, priv->base);
+	spin_unlock_irqrestore(&priv->lock, flags);
 
-	return (GPIO_READL(GPIO_DATA_IN, priv->base) >> gpio & 1);
+	return (val >> gpio & 1);
 }
 
 static void atcgpio_set(struct gpio_chip *gc, unsigned int gpio, int data)
 {
-	unsigned long val;
+	unsigned long flags;
 	struct atcgpio_priv *priv;
+	unsigned long val;
 
 	priv = gpiochip_get_data(gc);
-
+	spin_lock_irqsave(&priv->lock, flags);
 	if (data)
 		val = GPIO_READL(GPIO_DATA_OUT, priv->base) | (0x1UL << gpio);
 	else
 		val = GPIO_READL(GPIO_DATA_OUT, priv->base) & ~(0x1UL << gpio);
-
 	GPIO_WRITEL(val, GPIO_DATA_OUT, priv->base);
+	spin_unlock_irqrestore(&priv->lock, flags);
 }
 
 static int atcgpio_dir_in(struct gpio_chip *gc, unsigned int gpio)
@@ -117,12 +121,14 @@ static int atcgpio_dir_out(struct gpio_chip *gc, unsigned int gpio, int data)
 
 static void atcgpio_irq_ack(struct irq_data *data)
 {
-	struct atcgpio_priv *priv;
 	unsigned long flags;
+	struct atcgpio_priv *priv;
+	unsigned long irq;
 
 	priv = atcgpio_irq_data_get_data(data);
+	irq = priv->virq_base ? data->irq : data->hwirq;
 	spin_lock_irqsave(&priv->lock, flags);
-	GPIO_WRITEL(0x1UL << atcirq_to_gpio(data->irq), INT_STATE, priv->base);
+	GPIO_WRITEL(0x1UL << atcirq_to_gpio(irq), INT_STATE, priv->base);
 	spin_unlock_irqrestore(&priv->lock, flags);
 }
 
@@ -131,10 +137,12 @@ static void atcgpio_irq_unmask(struct irq_data *data)
 	unsigned long val;
 	unsigned long flags;
 	struct atcgpio_priv *priv;
-	
+	unsigned long irq;
+
 	priv = atcgpio_irq_data_get_data(data);
+	irq = priv->virq_base ? data->irq : data->hwirq;
 	spin_lock_irqsave(&priv->lock, flags);
-	val = GPIO_READL(INT_ENABLE, priv->base) | (0x1UL << atcirq_to_gpio(data->irq));
+	val = GPIO_READL(INT_ENABLE, priv->base) | (0x1UL << atcirq_to_gpio(irq));
 	GPIO_WRITEL(val, INT_ENABLE, priv->base);
 	spin_unlock_irqrestore(&priv->lock, flags);
 }
@@ -144,28 +152,29 @@ static void atcgpio_irq_mask(struct irq_data *data)
 	unsigned long val;
 	unsigned long flags;
 	struct atcgpio_priv *priv;
+	unsigned long irq;
 
 	priv = atcgpio_irq_data_get_data(data);
+	irq = priv->virq_base ? data->irq : data->hwirq;
 	spin_lock_irqsave(&priv->lock, flags);
-	val = GPIO_READL(INT_ENABLE, priv->base) & ~(0x1UL << atcirq_to_gpio(data->irq));
+	val = GPIO_READL(INT_ENABLE, priv->base) & ~(0x1UL << atcirq_to_gpio(irq));
 	GPIO_WRITEL(val, INT_ENABLE, priv->base);
 	spin_unlock_irqrestore(&priv->lock, flags);
 }
 
 static int atcgpio_irq_set_type(struct irq_data *data, unsigned int flow_type)
 {
-	unsigned long gpio = atcirq_to_gpio(data->irq);
-	unsigned long ch = gpio%8;
-	unsigned long mode_off = ch<<2;
-	unsigned long val;
+	struct atcgpio_priv *priv  = atcgpio_irq_data_get_data(data);
+	unsigned int irq = priv->virq_base ? data->irq : data->hwirq;
+	unsigned int gpio = atcirq_to_gpio(irq);
+	unsigned int ch = (gpio % 8);
+	unsigned int mode_off = (ch << 2);
+	unsigned int val;
 	unsigned long flags;
-	struct atcgpio_priv *priv;
-
-	priv = atcgpio_irq_data_get_data(data);
 
 	spin_lock_irqsave(&priv->lock, flags);
 	val = GPIO_READL(INT_MODE(gpio), priv->base);
-	val &= ~(7<<mode_off);
+	val &= ~(7 << mode_off);
 	if (flow_type & IRQF_TRIGGER_RISING && flow_type & IRQF_TRIGGER_FALLING)
 		GPIO_WRITEL(val | DUAL_EDGE<<mode_off, INT_MODE(gpio), priv->base);
 	else if (flow_type & IRQF_TRIGGER_FALLING)
@@ -186,49 +195,56 @@ static void gpio_irq_router(struct irq_desc *desc)
 	priv = irq_desc_get_handler_data(desc);
 	status = GPIO_READL(INT_STATE, priv->base);
 	status &= ~((1 << 22) | (1 << 25) | (1 << 26));
-
 	while (status) {
 		if (status & 0x1UL)
 			generic_handle_irq(gpio_to_irq(i));
 		status >>= 1;
 		i++;
 	}
-
 	desc->irq_data.chip->irq_eoi(&desc->irq_data);
 }
+
+static const struct irq_chip atcgpio_irqchip = {
+	.name = "ATCGPIO100_irq",
+	.irq_ack = atcgpio_irq_ack,
+	.irq_mask = atcgpio_irq_mask,
+	.irq_unmask = atcgpio_irq_unmask,
+	.irq_set_type = atcgpio_irq_set_type,
+	.flags = IRQCHIP_IMMUTABLE,
+	GPIOCHIP_IRQ_RESOURCE_HELPERS,
+};
 
 static int atcgpio100_gpio_probe(struct platform_device *pdev)
 {
 	int (*read_fixup)(void __iomem *addr, unsigned int val,
-		unsigned int shift_bits);
-	struct resource *res, *irq_res;
-	int ret, err;
+			  unsigned int shift_bits);
+	struct resource *res;
+	int ret;
 	struct atcgpio_priv *priv;
 	struct gpio_chip *gc;
-	struct irq_chip *ic;
+	struct gpio_irq_chip *girq;
+	struct device_node *node;
+	u32 ngpios;
 
 	priv = devm_kzalloc(&pdev->dev,	sizeof(*priv), GFP_KERNEL);
-
 	if (!priv)
 		return -ENOMEM;
-
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!res) {
 		dev_err(&pdev->dev, "failed to request atcgpio100 resource\n");
-		return -ENOENT;
+		return -ENXIO;
 	}
-
-	irq_res = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
-	if (!irq_res) {
+	platform_set_drvdata(pdev, priv);
+	ret = platform_irq_count(pdev);
+	priv->irq_parent = platform_get_irq(pdev, 0);
+	if (priv->irq_parent < 0) {
 		dev_err(&pdev->dev, "failed to request atcgpio100 irq\n");
-		return -ENOENT;
+		return -ENXIO;
 	}
-
 	priv->virq_base = ATCGPIO100_VIRTUAL_IRQ_BASE;
 	priv->base = devm_ioremap_resource(&pdev->dev, res);
 	spin_lock_init(&priv->lock);
-
 	if (IS_ERR((void *)priv->base))
 		return PTR_ERR((void *)priv->base);
 
@@ -236,10 +252,19 @@ static int atcgpio100_gpio_probe(struct platform_device *pdev)
 	read_fixup = symbol_get(readl_fixup);
 	ret = read_fixup(priv->base, 0x020310, 8);
 	symbol_put(readl_fixup);
-	if (!ret){
-		dev_err(&pdev->dev, "failed read ID register,bitmap not support gpio100\n");
+	if (!ret) {
+		dev_err(&pdev->dev,
+			"Failed to read ID register, ATCGPIO100 is not supported.\n");
 		return -ENXIO;
 	}
+	
+	node = pdev->dev.of_node;
+	if (!node)
+		return -EINVAL;
+	
+	ret = of_property_read_u32(node, "ngpios", &ngpios);
+	if (ret)
+		ngpios = DEFAULT_PIN_NUMBER;
 
 	/* disable interrupt */
 	GPIO_WRITEL(0x00000000UL, INT_ENABLE, priv->base);
@@ -254,34 +279,29 @@ static int atcgpio100_gpio_probe(struct platform_device *pdev)
 	gc->parent = &pdev->dev;
 	gc->label = "atcgpio100";
 	gc->base = 0;
-	gc->ngpio = 16;
+	gc->ngpio = ngpios;
 	gc->direction_output = atcgpio_dir_out;
 	gc->direction_input = atcgpio_dir_in;
 	gc->set = atcgpio_set;
 	gc->get = atcgpio_get;
-	gc->to_irq = atcgpio_to_irq;
-	ic = &priv->chip;
-	ic->name = "ATCGPIO100_irq";
-	ic->irq_ack = atcgpio_irq_ack;
-	ic->irq_mask = atcgpio_irq_mask;
-	ic->irq_unmask = atcgpio_irq_unmask;
-	ic->irq_set_type = atcgpio_irq_set_type;
+	girq = &priv->gc.irq;
+	gpio_irq_chip_set_chip(girq, &atcgpio_irqchip);
+	girq->parent_handler = gpio_irq_router;
+	girq->num_parents = 1;
+	girq->first = priv->virq_base;
+	girq->parents = devm_kcalloc(&pdev->dev, girq->num_parents,
+				     sizeof(*girq->parents), GFP_KERNEL);
+	girq->parents[0] = priv->irq_parent;
+	if (!girq->parents)
+		return -ENOMEM;
 
-	err = gpiochip_add_data(gc, priv);
-	if (err < 0)
-		return err;
-
-	platform_set_drvdata(pdev, priv);
-	
-	ret = gpiochip_irqchip_add(&priv->gc, ic,priv->virq_base,
-		handle_level_irq,IRQ_TYPE_NONE);
-
+	girq->default_type = IRQ_TYPE_NONE;
+	girq->handler = handle_level_irq;
+	ret = devm_gpiochip_add_data(&pdev->dev, &priv->gc, priv);
 	if (ret) {
-		dev_err(&pdev->dev, "could not add irqchip\n");
-		gpiochip_remove(&priv->gc);
+		dev_err(&pdev->dev, "gpiochip_add error %d\n", ret);
 		return ret;
 	}
-	gpiochip_set_chained_irqchip(&priv->gc, ic,irq_res->start, gpio_irq_router);
 	pr_info("ATCGPIO100 module inserted\n");
 
 	return 0;
@@ -295,7 +315,7 @@ static int atcgpio100_gpio_remove(struct platform_device *pdev)
 	GPIO_WRITEL(0x00000000UL, INT_ENABLE, priv->base);
 	gpiochip_remove(&priv->gc);
 	pr_info("GPIO module removed\n");
-	
+
 	return 0;
 }
 
